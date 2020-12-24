090328c2f46ea7dbad0cf3ec09d783f33617d5b3
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-12-24 21:02:50 +0800

    feat: add input item

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from "./components/header";
 import Footer from "./components/footer";
+import InputItem from "./components/inputItem";
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <div className={'app'}>
       <Header/>
+      <InputItem />
       <Footer/>
     </div>
   );
---
+++ src/components/inputItem/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Input } from 'antd'
+import './index.css'
+
+const InputItem = () => {
+    return(
+    <Input className={'input'} placeholder={'What needs to be done?'} />
+    )
+}
+
+export default InputItem
