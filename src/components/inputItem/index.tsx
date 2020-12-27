fae2ff564b4c0e56f244da269014025fc486d24c
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-12-27 19:22:25 +0800

    feat: add todo list

--- src/components/inputItem/index.tsx
+++ src/components/inputItem/index.tsx
@@ -1,11 +1,52 @@
 import React from 'react';
-import { Input } from 'antd'
+import { Input, List } from 'antd'
 import './index.css'
 
-const InputItem = () => {
-    return(
-    <Input className={'input'} placeholder={'What needs to be done?'} />
-    )
+interface ITodoItem {
+    label:string,
+    checked:boolean,
+}
+
+interface isState {
+    todos: ITodoItem[],
+}
+
+class InputItem extends React.Component<any,isState> {
+    constructor(props:any) {
+        super(props);
+        this.state = {
+            todos: [],
+        };
+    }
+    handlePressEnter = (e:any) => {
+        const { todos } = this.state;
+        let arr = todos;
+        arr.push({ label: e.target.value, checked: false});
+        this.setState({ todos: arr});
+    }
+
+    render() {
+        return(
+            <div className={'mainInput'}>
+               <Input
+                   className={'input'}
+                    placeholder={'What needs to be done?'}
+                    onPressEnter={ e => this.handlePressEnter(e)}
+                />
+
+                <List>
+                    {this.state.todos.map((todo) => {
+                        return (
+                                <List.Item>
+                                    <label>
+                                        {todo.label}
+                                    </label>
+                                </List.Item>
+                            )
+                    })}
+                </List>
+            </div>
+        )}
 }
 
 export default InputItem
