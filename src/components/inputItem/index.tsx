a853fe4e2cea754ed3e50c250608b492f05334fc
hellolilia <lilia365mail@gmail.com>
2021-01-07 15:56:29 +0800

    feat: add clear completed button layout

--- src/components/inputItem/index.tsx
+++ src/components/inputItem/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { connect, useDispatch } from 'react-redux'
 import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
 import { store } from '../../store'
-import { Input } from 'antd'
+import { Button, Input } from 'antd'
 import './index.css'
 import AllTodos from './components/allTodos'
 import ActiveTodos from './components/activeTodos'
@@ -72,7 +72,7 @@ const InputItem = () => {
     <Router>
       <div className={'mainInput'}>
         <div className={'inputItem'}>
-          {todos && todos.length !== 0 ? (
+          {todos.filter((todo) => !todo.deleted).length !== 0 ? (
             <Input
               className='checkAll'
               type='checkbox'
@@ -88,7 +88,7 @@ const InputItem = () => {
             onPressEnter={(e) => handlePressEnter(e)}
           />
         </div>
-        {todos && todos.length !== 0 ? (
+        {todos.filter((todo) => !todo.deleted).length !== 0 ? (
           <div>
             <Switch>
               <Route exact path='/' component={AllTodos} />
@@ -96,11 +96,25 @@ const InputItem = () => {
               <Route path='/Complete' component={CompletedTodos} />
             </Switch>
             <div className={'bottomBorderOne'}>
-              <p>{count} items left</p>
-              <Link to='/'>All</Link>
-              <Link to='/Active'>Active</Link>
-              <Link to='/Complete'>Complete</Link>
-              <p onClick={clearCompleted}>Clear completed</p>
+              <div className={'countItems'}>
+                <p>{count} items left</p>
+              </div>
+              <div className={'filters'}>
+                <Link to='/'>All</Link>
+                <Link to='/Active'>Active</Link>
+                <Link to='/Complete'>Complete</Link>
+              </div>
+              <div className={'completed'}>
+                {todos.filter((todo) => todo.checked && !todo.deleted)
+                  .length === 0 ? null : (
+                  <Button
+                    onClick={clearCompleted}
+                    className={'completedButton'}
+                  >
+                    Clear completed
+                  </Button>
+                )}
+              </div>
             </div>
             <div className={'bottomBorderTwo'} />
             <div className={'bottomBorderThree'} />
